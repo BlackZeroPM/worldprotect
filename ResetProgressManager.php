2ed66d669a5c3430011207cb631e46521bd624eb
BlackZeroPM <pocketminevnp@gmail.com>
2018-03-30 15:48:07 +0700

    Update ResetProgressManager.php

--- ResetProgressManager.php
+++ ResetProgressManager.php
@@ -24,7 +24,7 @@ class ResetProgressManager{
     public function notifyProgress(string $progress, string $mineName){
         if(isset($this->subscriptions[$mineName])){
             foreach ($this->subscriptions[$mineName] as $sender){
-                $sender->sendConsole("RESET {$mineName}: {$progress}");
+                $sender->sendPopup("RESET {$mineName}: {$progress}");
             }
         }
     }
